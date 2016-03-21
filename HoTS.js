2dc2fe979b11ce78e83c16ca8822948e31710ab1
Kyle Miller <miller.kwill@gmail.com>
2016-03-21 17:49:07 -0600

    Day 1 progress

---
+++ HoTS.js
@@ -0,0 +1,41 @@
+(function() {
+  'use strict';
+
+  angular.module('HeroesOfTheStormApp', ['ui.router'])
+    .config(MainRouter)
+    .controller('HeroesController', HeroesController)
+// Initilizing the Ui-router for making a SPA
+    function MainRouter($stateProvider, $urlRouterProvider) {
+      $stateProvider
+      .state('LandingPage', {
+        url: "/",
+        templateUrl: "./LandingPage.html",
+      })
+
+      .state('HomePage', {
+        url: "/HomePage",
+        templateUrl: "./HomePage.html",
+      })
+
+      .state('HeroBuild', {
+        url: '/HeroBuild',
+        templateUrl: './HeroBuild.html'
+      })
+      $urlRouterProvider.otherwise('/')
+    }
+
+  function HeroesController($http) {
+    var hots = this
+
+
+    $http.get('HoTS.json')
+      .success(function(response) {
+        console.log(response.heroes)
+        hots.heroes = response.heroes
+      })
+  }
+
+
+
+
+}());
