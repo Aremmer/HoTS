e751675e6a17d4f5a723033d3bf3fa116234f8f9
Kyle Miller <miller.kwill@gmail.com>
2016-03-22 16:25:41 -0600

    Day 2 push

--- HoTS.js
+++ HoTS.js
@@ -4,25 +4,25 @@
   angular.module('HeroesOfTheStormApp', ['ui.router'])
     .config(MainRouter)
     .controller('HeroesController', HeroesController)
-// Initilizing the Ui-router for making a SPA
-    function MainRouter($stateProvider, $urlRouterProvider) {
-      $stateProvider
+    // Initilizing the Ui-router for making a SPA
+  function MainRouter($stateProvider, $urlRouterProvider) {
+    $stateProvider
       .state('LandingPage', {
         url: "/",
         templateUrl: "./LandingPage.html",
       })
 
-      .state('HomePage', {
-        url: "/HomePage",
-        templateUrl: "./HomePage.html",
-      })
+    .state('HomePage', {
+      url: "/HomePage",
+      templateUrl: "./HomePage.html",
+    })
 
-      .state('HeroBuild', {
-        url: '/HeroBuild',
-        templateUrl: './HeroBuild.html'
-      })
-      $urlRouterProvider.otherwise('/')
-    }
+    .state('HeroBuild', {
+      url: '/HeroBuild',
+      templateUrl: './HeroBuild.html'
+    })
+    $urlRouterProvider.otherwise('/')
+  }
 
   function HeroesController($http) {
     var hots = this
@@ -32,7 +32,24 @@
       .success(function(response) {
         console.log(response.heroes)
         hots.heroes = response.heroes
+
+        hots.roleFilters = []
+
+        function getRole() {
+          return (heroe.filter(function(arrEl) {
+            for (var i = 0; i < roleFilters.length; i++) {
+              if (hero.role === roleFilters[i]) {
+                return true
+              } else {
+                return false
+              }
+            }
+          }))
+        }
+
       })
+
+
   }
 
 
